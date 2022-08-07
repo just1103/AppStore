feb8bc2513ca0b764fe58ef341f382d2bfafdb7d
Applecider <hyojuson1103@gmail.com>
2022-08-07 23:37:44 +0900

    test: AppLookupAPI가 정상작동 테스트코드 추가

--- IdusIntroductionTests/NetworkProviderTests.swift
+++ IdusIntroductionTests/NetworkProviderTests.swift
@@ -38,6 +38,37 @@ class NetworkProviderTests: XCTestCase {
             XCTAssertNotNil(searchResultDTO)
             XCTAssertEqual(searchResultDTO.resultCount, 1)
             
+            let appItemDTO = searchResultDTO.results.first!
+            let appItem = AppItem.convert(appItemDTO: appItemDTO)
+            XCTAssertEqual(appItem.trackName, "아이디어스(idus)")
+            XCTAssertEqual(appItem.artistName, "Backpackr Inc.")
+            
+            expectation.fulfill()
+        })
+        .store(in: &cancellableBag)
+        
+        wait(for: [expectation], timeout: 10.0)
+    }
+    
+    func test_AppLookupAPI가_정상작동_하는지() throws {
+        let expectation = XCTestExpectation(description: "AppLookupAPI 비동기 테스트")
+       
+        let kakaoTalkAppID = "362057947"
+        let publisher = sut.fetchData(
+            api: IdusIntroductionURL.AppLookupAPI(appID: kakaoTalkAppID),
+            decodingType: SearchResultDTO.self
+        )
+        
+        publisher.sink(receiveCompletion: { _ in
+        }, receiveValue: { searchResultDTO in
+            XCTAssertNotNil(searchResultDTO)
+            XCTAssertEqual(searchResultDTO.resultCount, 1)
+            
+            let appItemDTO = searchResultDTO.results.first!
+            let appItem = AppItem.convert(appItemDTO: appItemDTO)
+            XCTAssertEqual(appItem.trackName, "KakaoTalk")
+            XCTAssertEqual(appItem.artistName, "Kakao Corp.")
+            
             expectation.fulfill()
         })
         .store(in: &cancellableBag)
